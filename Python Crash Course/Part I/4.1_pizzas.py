7b7429d456b899defb8d26576d99b04b527027c9
Pavel Shuravin <paulshuravin@gmail.com>
2020-10-14 18:44:03 +0300

    Several list methods

---
+++ Python Crash Course/Part I/4.1_pizzas.py
@@ -0,0 +1,6 @@
+pizza = ["Dodo", "Dominos", "Papa John's", "Allo Pizza", "Pizza Hut"]
+
+for item in pizza:
+    print("I like pizza from", item)
+
+print("I really love pizza!")
---
+++ Python Crash Course/Part I/4_list_coompr.py
@@ -0,0 +1,21 @@
+# Counting to twenty
+numbers = []
+for num in range(1, 21):
+    numbers.append(num)
+
+# print(numbers)
+
+
+# One Million
+million = list(range(1, 1000001))
+# for num in million:
+#     print(num, sep=" ")
+
+
+# Summing
+print(sum(million))
+
+
+# Cube COmprehension
+cubes = [n ** 3 for n in range(0, 11)]
+print(cubes)
